f59ffe4c83b9aa0d229bcec5806287bdad6174ec
Ubisoft-PC\Ubisoft <anthony39.david@gmail.com>
2025-11-19 22:13:50 -0500

    Prepare shadow for helicopter.

--- Assets/Content/Scripts/Controllers/HelicopterController.cs
+++ Assets/Content/Scripts/Controllers/HelicopterController.cs
@@ -10,6 +10,8 @@ public class HelicopterController : MonoBehaviour
     public float moveSpeed = 10f;        // Move speed (unit / second)
     public float rotationSpeed = 5f;     // Speed roation to aim at the move direction
 
+    public float height = 0f;
+    public float climbSpeed = 3f;
     private GaugeController gauge;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -45,6 +47,15 @@ public class HelicopterController : MonoBehaviour
             Quaternion targetRot = Quaternion.Euler(0f, 0f, angle);
             Body.rotation = Quaternion.Slerp(Body.rotation, targetRot, rotationSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            height = Mathf.Min(height + climbSpeed * Time.deltaTime, 10f);
+        }            
+        else
+        {
+            height = Mathf.Max(height - climbSpeed * Time.deltaTime, 0f);
+        }        
 
         float fuelBurned = Time.deltaTime + input.magnitude * Time.deltaTime; // Decrease fuel based on movement
         gauge.ConsumeValue(fuelBurned); // Update gauge with animated fuel value
